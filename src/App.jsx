aec1df9dbe34037c2e7dd27ae63501d210e49af2
Gohans Tapia <gohans.tapia@inacapmail.cl>
2026-06-24 12:42:49 -0400

    feat: proyecto base de datos del album

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { stickers } from "./data/stickers";
+
+function App() {
+  console.log(stickers);
+
+  return (
+    <>
+      <h1>Álbum Mundial 2026</h1>
+    </>
+  );
+}
+
+export default App;
